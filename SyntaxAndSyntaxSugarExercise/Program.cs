970dbfe6de27e248c93b53824680c98ba08b62da
Audryana Kelley <audryanakelley@gmail.com>
2024-07-12 09:59:41 -0500

    added updates to exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.ComponentModel.Design;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int answer = 4;
+            string response;
+            if (answer < 9)
+            
+            { response = answer + " is less than nine"; 
+            }
+            else
+            {
+                response = answer  + " is greater than nine";
+            }
+            Console.WriteLine(response);
         }
+        
+        
+        
     }
 }
